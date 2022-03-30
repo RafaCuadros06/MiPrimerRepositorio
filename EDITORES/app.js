2732d457a47676c906267884b9310e7e84327b57
RafaCuadros06 <101266846+RafaCuadros06@users.noreply.github.com>
2022-03-30 09:04:12 -0500

    commit2

---
+++ EDITORES/app.js
@@ -0,0 +1,12 @@
+/* Aplicacion que me guarde los datos en una tabla*/
+var nombre;
+var apellido;
+var telefono;
+var email;
+var direccion;
+var fecha;
+var nota;
+var tabla= document.getElementById("tabla");
+var boton= document.getElementById("guardar");
+var contador = 1;
+console.log("Javascript funcionando")
