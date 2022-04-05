db47f727e35a3800615b771338402895458346a2
RafaCuadros06 <101266846+RafaCuadros06@users.noreply.github.com>
2022-04-05 08:34:27 -0500

    commit2

--- EDITORES/app.js
+++ EDITORES/app.js
@@ -13,3 +13,39 @@ var contador = 1;
 guardar.addEventListener("click",function(){
     console.log("se ha dado click al boton");
 })
+
+function agregarATabla (){
+nombre = document.getElementById("nombre").value;
+apellido = document.getElementById("apellido").value;
+telefono = document.getElementById("telefono").value;
+email = document.getElementById("email").value;
+direccion = document.getElementById("direccion").value;
+fecha = document.getElementById("fecha").value;
+nota = document.getElementById("nota").value;
+console.log(nombre);
+console.log(apellido);
+console.log(telefono);
+console.log(email);
+console.log(direccion);
+console.log(fecha);
+console.log(nota);
+var fila = tabla.insertRow(contador);
+var celda1 = fila.insertCell(0);
+var celda2 = fila.insertCell(1);
+var celda3 = fila.insertCell(2);
+var celda4 = fila.insertCell(3);
+var celda5 = fila.insertCell(4);
+var celda6 = fila.insertCell(5);
+var celda7 = fila.insertCell(6);
+var celda8 = fila.insertCell(7);
+celda1.innerHTML=nombre;
+celda2.innerHTML=apellido;
+celda3.innerHTML=telefono;
+celda4.innerHTML=email;
+celda5.innerHTML=direccion;
+celda6.innerHTML=fecha;
+celda7.innerHTML=nota;
+celda18.innerHTML= '<button class= "btn_danger" onclick="eliminar(this)>Borrar</button>"';
+contador++;
+
+}
